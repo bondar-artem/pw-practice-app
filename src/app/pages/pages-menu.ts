c266c022e9599c58ef13d516ba4e4c2c81bb0fad
Artem Bondar <artem.bondar@gmail.com>
2023-04-27 17:01:43 -0500

    dashboard removed

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,12 +1,7 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
-    link: '/pages/dashboard',
-    home: true,
-  },
+  
   {
     title: 'IoT Dashboard',
     icon: 'home-outline',
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,17 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
-    {
-      path: 'dashboard',
-      component: ECommerceComponent,
-    },
     {
       path: 'iot-dashboard',
       component: DashboardComponent,
