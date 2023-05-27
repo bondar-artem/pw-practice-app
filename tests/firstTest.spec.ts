2274ccc4928e638194684dc77e446e98f4be9ae2
Artem Bondar <artem.bondar@gmail.com>
2023-05-27 16:16:56 -0500

    locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,8 +1,37 @@
 import {test} from '@playwright/test'
 
 
-test('the first test', async ({page}) => {
+test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules', async({page}) => {
+    //by Tag name
+    await page.locator('input').first().click()
+
+    //by ID
+    page.locator('#inputEmail1')
+
+    //by Class value
+    page.locator('.shape-rectangle')
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    // by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    // by partial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
 })
