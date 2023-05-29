f92eafdc254b33a1f7d1cba24d84553937787551
Artem Bondar <artem.bondar@gmail.com>
2023-05-29 16:51:50 -0500

    child elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -50,3 +50,12 @@ test('User facing locators', async({page}) => {
 
     await page.getByTitle('IoT Dashboard').click()
 })
+
+test('locating child elements', async({page}) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click()
+})
