aad6256c52ebe361f082f0eb6c644e1f6e22be22
Artem Bondar <artem.bondar@gmail.com>
2023-05-30 18:08:11 -0500

    parent elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -59,3 +59,16 @@ test('locating child elements', async({page}) => {
 
     await page.locator('nb-card').nth(3).getByRole('button').click()
 })
+
+test('locating parent elements', async({page}) => {
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click()
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click()
+
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"})
+        .getByRole('textbox', {name: "Email"}).click()
+
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
+})
