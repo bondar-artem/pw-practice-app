88ecacf62a2b357735ea3a2f1c0e7dd3a29b4163
Artem Bondar <artem.bondar@gmail.com>
2023-06-01 17:36:14 -0500

    reusing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {test, expect} from '@playwright/test'
 
 
 test.beforeEach(async({page}) => {
@@ -71,4 +71,16 @@ test('locating parent elements', async({page}) => {
         .getByRole('textbox', {name: "Email"}).click()
 
     await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
+})
+
+test('Reusing the locators', async({page}) => {
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+
+    await emailField.fill('test@test.com')
+    await basicForm.getByRole('textbox', {name: "Password"}).fill('Welcome123')
+    await basicForm.locator('nb-checkbox').click()
+    await basicForm.getByRole('button').click()
+
+    await expect(emailField).toHaveValue('test@test.com')
 })
