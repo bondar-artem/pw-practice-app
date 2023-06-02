a4b99bfa52d84b4cc07a273720198a8823986ace
Artem Bondar <artem.bondar@gmail.com>
2023-06-01 22:11:57 -0500

    extract values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -84,3 +84,23 @@ test('Reusing the locators', async({page}) => {
 
     await expect(emailField).toHaveValue('test@test.com')
 })
+
+test('extracting values', async({page}) => {
+    //single test value
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"})
+    const buttonText = await basicForm.locator('button').textContent()
+    expect(buttonText).toEqual('Submit')
+
+    //all text values
+    const allRadioButtonsLabels = await page.locator('nb-radio').allTextContents()
+    expect(allRadioButtonsLabels).toContain("Option 1")
+
+    //input value
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+    await emailField.fill('test@test.com')
+    const emailValue = await emailField.inputValue()
+    expect(emailValue).toEqual('test@test.com')
+
+    const placeholderValue = await emailField.getAttribute('placeholder')
+    expect(placeholderValue).toEqual('Email')
+})
