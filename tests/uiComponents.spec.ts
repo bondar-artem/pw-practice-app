2c6a34cd0e8fa89f9a4409f05b728d2c7f38397a
Artem Bondar <artem.bondar@gmail.com>
2023-06-09 16:37:31 -0500

    tooltips

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -84,3 +84,15 @@ test('lists and dropdowns', async({page}) => {
     }
 
 })
+
+test('tooltips', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const toolTipCard = page.locator('nb-card', {hasText: "Tooltip Placements"})
+    await toolTipCard.getByRole('button', {name: "Top"}).hover()
+
+    page.getByRole('tooltip') //if you have a role tooltip created
+    const tooltip = await page.locator('nb-tooltip').textContent()
+    expect(tooltip).toEqual('This is a tooltip')
+})
