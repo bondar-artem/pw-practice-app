2c7eb4b6d9115241ec4cef90d312a33459081de2
Artem Bondar <artem.bondar@gmail.com>
2023-06-09 21:08:24 -0500

    dialog boxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -96,3 +96,16 @@ test('tooltips', async({page}) => {
     const tooltip = await page.locator('nb-tooltip').textContent()
     expect(tooltip).toEqual('This is a tooltip')
 })
+
+test('dialog box', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+
+    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+})
