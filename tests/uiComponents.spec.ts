472a7b0510789cfada10758bd1ecb67d31a2e94f
Artem Bondar <artem.bondar@gmail.com>
2023-06-10 12:30:59 -0500

    web tables part 1

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -109,3 +109,25 @@ test('dialog box', async({page}) => {
     await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
     await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
 })
+
+test('web tables', async({page}) => {
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    //1 get the row by any test in this row
+    const targetRow = page.getByRole('row', {name: "twitter@outlook.com"})
+    await targetRow.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('Age').clear()
+    await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+    await page.locator('.nb-checkmark').click()
+
+    //2 get the row based on the value in the specific column
+    await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+    const targetRowById = page.getByRole('row', {name: "11"}).filter({has: page.locator('td').nth(1).getByText('11')})
+    await targetRowById.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+    await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+    await page.locator('.nb-checkmark').click()
+    await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+
+})
