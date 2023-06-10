9a1336c0d54af8f40a6a97ff7cfd03527bbb655a
Artem Bondar <artem.bondar@gmail.com>
2023-06-10 17:50:23 -0500

    web tables 2

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -130,4 +130,27 @@ test('web tables', async({page}) => {
     await page.locator('.nb-checkmark').click()
     await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
 
+    //3 test filter of the table
+
+    const ages = ["20", "30", "40", "200"]
+
+    for( let age of ages){
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500)
+        const ageRows = page.locator('tbody tr')
+        
+        for(let row of await ageRows.all()){
+            const cellValue = await row.locator('td').last().textContent()
+
+            if(age == "200"){
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+            } else {
+                expect(cellValue).toEqual(age)
+            }
+
+           
+        }
+    }
+
 })
