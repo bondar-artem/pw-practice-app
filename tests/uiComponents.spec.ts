bc4ee14d1660c3f943cbc1e20ba747465d0cb491
Artem Bondar <artem.bondar@gmail.com>
2023-06-20 21:54:12 -0500

    datepicker part two

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -159,6 +159,21 @@ test('datepicker', async({page}) => {
     const calendarInputField = page.getByPlaceholder('Form Picker')
     await calendarInputField.click()
 
-    await page.locator('[class="day-cell ng-star-inserted"]').getByText('1', {exact: true}).click()
-    await expect(calendarInputField).toHaveValue('Jun 1, 2023')
+    let date = new Date()
+    date.setDate(date.getDate() + 7)
+    const expectedDate = date.getDate().toString()
+    const expectedMonthShot = date.toLocaleString('En-US', {month: 'short'})
+    const expectedMonthLong = date.toLocaleString('En-US', {month: 'long'})
+    const expectedYear = date.getFullYear()
+    const dateToAssert = `${expectedMonthShot} ${expectedDate}, ${expectedYear}`
+
+    let calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`
+    while(!calendarMonthAndYear.includes(expectedMonthAndYear)){
+        await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
+        calendarMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+    }
+
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click()
+    await expect(calendarInputField).toHaveValue(dateToAssert)
 })
