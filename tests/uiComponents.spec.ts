66b4c7201a772c6998722321ff1add0d365853e3
Artem Bondar <artem.bondar@gmail.com>
2023-06-21 21:43:45 -0500

    sliders

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -177,3 +177,27 @@ test('datepicker', async({page}) => {
     await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact: true}).click()
     await expect(calendarInputField).toHaveValue(dateToAssert)
 })
+
+test('sliders', async({page}) => {
+    // Update attribute
+    const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+    await tempGauge.evaluate( node => {
+        node.setAttribute('cx', '232.630')
+        node.setAttribute('cy', '232.630')
+    })
+    await tempGauge.click()
+
+    //Mouse movement
+    const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x, y)
+    await page.mouse.down()
+    await page.mouse.move(x +100, y)
+    await page.mouse.move(x+100, y+100)
+    await page.mouse.up()
+    await expect(tempBox).toContainText('30')
+})
