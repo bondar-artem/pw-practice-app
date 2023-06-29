58970a8e20c23f52111003d25bf9d345fe3ff286
Artem Bondar <artem.bondar@gmail.com>
2023-06-29 16:53:02 -0500

    first page object

---
+++ page-objects/navigationPage.ts
@@ -0,0 +1,16 @@
+import { Page } from "@playwright/test";
+
+export class NavigationPage {
+
+    readonly page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async formLayoutsPage(){
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Form Layouts').click()
+    }
+
+}
---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test'
+import {NavigationPage} from '../page-objects/navigationPage'
+
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test('navigate to form page', async({page}) => {
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.formLayoutsPage()
+})
