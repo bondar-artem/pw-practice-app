0ddfdd48210f3bd5949b72340706113aeb115384
Artem Bondar <artem.bondar@gmail.com>
2023-07-03 09:00:20 -0500

    form layouts page

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,33 @@
+import { Page } from "@playwright/test";
+
+export class FormLayoutsPage {
+
+    private readonly page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async submitUsingTheGrigdFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string){
+        const usingTheGridForm = this.page.locator('nb-card', {hasText: "Using the Grid"})
+        await usingTheGridForm.getByRole('textbox', {name: "Email"}).fill(email)
+        await usingTheGridForm.getByRole('textbox', {name: "Password"}).fill(password)
+        await usingTheGridForm.getByRole('radio', {name: optionText}).check({force: true})
+        await usingTheGridForm.getByRole('button').click()
+    }
+
+    /**
+     * This method fill out the Inline form with user details
+     * @param name - should be first and last name
+     * @param email - valid email for the test user
+     * @param rememberMe - true or false if user session to be safed
+     */
+    async sumbitInlineFormWithNameEmailAndCheckbox(name: string, email: string, rememberMe: boolean){
+        const inlineForm = this.page.locator('nb-card', {hasText: "Inline form"})
+        await inlineForm.getByRole('textbox', {name: "Jane Doe"}).fill(name)
+        await inlineForm.getByRole('textbox', {name: "Email"}).fill(email)
+        if(rememberMe)
+            await inlineForm.getByRole('checkbox').check({force: true})
+        await inlineForm.getByRole('button').click()
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
 
 
 test.beforeEach(async({page}) => {
@@ -13,4 +14,13 @@ test('navigate to form page', async({page}) => {
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+})
+
+test('parametrized methods', async({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutsPage = new FormLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutsPage.submitUsingTheGrigdFormWithCredentialsAndSelectOption('tesdt@test.com', 'Welcome1', 'Option 2')
+    await onFormLayoutsPage.sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
 })
