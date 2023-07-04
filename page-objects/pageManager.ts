9eaf19828f56f9e2f2373f8501838807d5a10285
Artem Bondar <artem.bondar@gmail.com>
2023-07-04 11:40:15 -0500

    page manager

---
+++ page-objects/pageManager.ts
@@ -0,0 +1,33 @@
+import { Page, expect } from "@playwright/test";
+import { NavigationPage } from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
+import { DatepickerPage } from '../page-objects/datepickerPage'
+
+
+export class PageManager{
+
+    private readonly page: Page
+    private readonly navigationPage: NavigationPage
+    private readonly formLayoutsPage: FormLayoutsPage
+    private readonly datepickerPage: DatepickerPage
+
+    constructor(page: Page){
+        this.page = page
+        this.navigationPage = new NavigationPage(this.page)
+        this.formLayoutsPage = new FormLayoutsPage(this.page)
+        this.datepickerPage = new DatepickerPage(this.page)
+    }
+
+    navigateTo(){
+        return this.navigationPage
+    }
+
+    onFormLayoutsPage(){
+        return this.formLayoutsPage
+    }
+
+    onDatepickerPage(){
+        return this.datepickerPage
+    }
+
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,7 +1,5 @@
 import {test, expect} from '@playwright/test'
-import {NavigationPage} from '../page-objects/navigationPage'
-import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-import { DatepickerPage } from '../page-objects/datepickerPage'
+import { PageManager } from '../page-objects/pageManager'
 
 
 test.beforeEach(async({page}) => {
@@ -9,23 +7,20 @@ test.beforeEach(async({page}) => {
 })
 
 test('navigate to form page', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-    await navigateTo.datepickerPage()
-    await navigateTo.smartTablePage()
-    await navigateTo.toastrPage()
-    await navigateTo.tooltipPage()
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
 })
 
 test('parametrized methods', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutsPage = new FormLayoutsPage(page)
-    const onDatepickerPage = new DatepickerPage(page)
-
-    await navigateTo.formLayoutsPage()
-    await onFormLayoutsPage.submitUsingTheGrigdFormWithCredentialsAndSelectOption('tesdt@test.com', 'Welcome1', 'Option 2')
-    await onFormLayoutsPage.sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
-    await navigateTo.datepickerPage()
-    await onDatepickerPage.selectCommonDatePickerDateFromToday(10)
-    await onDatepickerPage.selectDatepickerWithRangeFromToday(6, 10)
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGrigdFormWithCredentialsAndSelectOption('tesdt@test.com', 'Welcome1', 'Option 2')
+    await pm.onFormLayoutsPage().sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
+    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
 })
