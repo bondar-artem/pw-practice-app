c83868b6bf93c2ad0823d401c31ace9e6746d307
Marcscob <marcscob@hotmail.com>
2023-12-17 23:31:21 -0300

    h

--- tests/TiposDeTestes.ts
+++ tests/TiposDeTestes.ts
@@ -1,4 +1,28 @@
 import {test} from '@playwright/test'
+//para evitar repetição de linhas, criamos hooks para linhas comuns a diferentes testes
+//hook before each test
+test.beforeEach(async({page})=>{
+    await page.goto('http://localhost:4200')
+    await page.getByText("Forms").click()
+    await page.getByText("Form Layouts").click()
+})
+
+
+//hook before all tests as a general precondition before all test. Not very used
+//test.beforeEach(()=>{})
+
+//testes unitários
+// test("Form Layouts", async({ page }) => {
+//     await page.getByText("Form Layouts").click()
+
+// });
+// test("datepicker page", async({ page }) => {
+//     await page.getByText("Datepicker").click()
+
+// });
+
+//another hooks are afterEach and afterAll
+//test.afterEach(()=>{})
 
 //testes unitários
 test("some describing string", ()=>{
--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,8 +1,42 @@
 import { test } from "@playwright/test";
 
-//testes unitários
-test("some describing string", async({ page }) => {
+test.beforeEach(async({page})=>{
     await page.goto('http://localhost:4200')
-await page.getByText("Forms").click()
-await page.getByText("Form Layouts").click()
-});
+    await page.getByText("Forms").click()
+    await page.getByText("Form Layouts").click()
+})
+
+test('locator syntax rules', async({page})=>{ 
+    //note that the locators are useless without an action and await 'cause an action is a promise, a locator is not
+
+    //by tag name
+   //await page.locator('input').click() //return all inputs so the click will return an error, but...
+
+   await page.locator('input').first().click() //will return ok
+
+    //by ID
+     page.locator('#inputEmail1')
+
+    //by class
+    page.locator('.shape-rectangle')//return all inputs with this class
+
+    //by atribute
+    page.locator('[placeholder="Email"]')
+
+    //by full class value
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    //combine different selectors. no space between selectors
+    page.locator('input[placeholder="Email"][nbinput]')
+
+    //by xpath (not recommended)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //parcial text match
+    page.locator(':text("Using")')
+
+    //by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+
+})
