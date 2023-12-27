ffc63454be11b20bfd0249549d69fd7ad1fbebdb
Marcscob <marcscob@hotmail.com>
2023-12-27 18:28:20 -0300

    ok

--- tests/legnetTest1.spec.ts
+++ tests/legnetTest1.spec.ts
@@ -6,23 +6,16 @@ import { expect, test } from "@playwright/test";
 test("Login", async ({ page }) => {
   await page.goto("https://www.legnet.com.br/legnet/login.php");
   const cod_cli = page.locator("#login_cod_cliente");
-  await cod_cli.fill("1");
+  await cod_cli.fill("2106");
   const user_name = page.locator("#login_usuario");
   await user_name.fill("juridico");
   const login_senha = page.locator("#login_senha");
   await login_senha.fill("Legnet123!");
   await page.getByRole("button", { name: "ENTRAR" }).first().click();
-  await page.click('#fa-plus-topo');
-  const fourthDiv = await page.$('#menubase > div:nth-child(3)');
-  await fourthDiv.hover();
-  await page.click(".space")
-
-  
-
-  
- 
-
-
+  // await page.click('#fa-plus-topo');
+  // const fourthDiv = await page.$('#menubase > div:nth-child(3)');
+  // await fourthDiv.hover();
+  //await page.click(".space")
   
 })
 
