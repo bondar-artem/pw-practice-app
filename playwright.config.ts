a2cbc7c119ed732137a05441cfae00b28162bfbd
MaryanaDovhanyk <mdovhanyk@seluxdx.co>
2025-02-26 09:54:50 +0200

    redused unused code

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -59,7 +59,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    }, */
 
     /* Test against mobile viewports. */
     // {
