d371403d4f01db2ab6cbd1968645bf5acd03e374
Tatyana Tanasiychuk <tatyana.tanasiychuk@gmail.com>
2025-05-15 10:43:00 -0500

    firstTest.spec.ts

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,6 +2,8 @@ import {test} from '@playwright/test';
 
 test('the first test', async ({page}) => {
     await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts'). click();
 })
 
 // test.describe('test suite 1)', () => {
