0d240b47a07451ffd682843fbe55d50654ffe6fe
Tatyana T <tatyana.tanasiychuk@gmail.com>
2025-06-24 12:45:20 -0500

    created txt file with onw message

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,29 +1,15 @@
 import {test} from '@playwright/test';
 
-test.beforeEach(async({page}) => {
+test.beforeEach(async({page}) =>{
     await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
 })
 
-test.describe('suite1', () => {
-    test.beforeEach(async({page}) => {
-        await page.getByText('Charts').click();
-    })
-    test('the first test', async ({page}) => {
-        await page.getByText('Form Layouts').click();
-    })
-    test('navigate to datapicker page', async ({page}) => {
-        await page.getByText('Datapicker').click();
-    })
+test('the first test', async({page}) => {
+ 
+    await page.getByText('Form Layouts').click()
 })
 
-test.describe('suite2', () => {
-    test.beforeEach(async({page}) => {
-        await page.getByText('Forms').click();
-    })
-    test('the first test2', async ({page}) => {
-        await page.getByText('Form Layouts').click();
-    })
-    test('navigate to datapicker page', async ({page}) => {
-        await page.getByText('Datapicker').click();
-    })
+test('navigate to datepicker page', async({page}) => {
+    await page.getByText('Datepicker').click()
 })
