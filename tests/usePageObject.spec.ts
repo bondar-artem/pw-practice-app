8fb059b6aacf64bcede4e46eca12a557bf5083aa
mason <minh.nthcs@gmail.com>
2025-07-24 21:49:04 +0700

    update: method parameterized

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { NavigationPage } from '../pages/navigationPage';
+import { ForLayoutsPage } from '../pages/formLayoutsPage';
 
 test.beforeEach(async ({page}) => {
     await page.goto("http://localhost:4200")
@@ -12,4 +13,13 @@ test('navigate to Form Layout', async ({page}) => {
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+})
+
+test('method parameterized', async ({page}) => {
+    const formLayoutsPage = new ForLayoutsPage(page)
+    const navigateTo = new NavigationPage(page)
+
+    await navigateTo.FormLayoutsPage()
+    await formLayoutsPage.inputFormUsingGridsWithCredentials('test@gamil.com', 'myPassword', 'Option 1')
+    await formLayoutsPage.inputFormUsingInlineForm('Minh', 'masson@gmail.com', true)
 })
